6b9080c5ff917912ffd657c7e9575c266854013e
Skochynskiy Dmytro <skodmy@gmail.com>
2017-12-27 13:17:50 +0200

    WhatIsMyBrowserCrawler completed

--- plateye/usragent/crawlers.py
+++ plateye/usragent/crawlers.py
@@ -1,23 +1,37 @@
-#!/usr/bin/env python
+"""
 
+"""
+import os
 import sys
 from abc import ABC, abstractmethod
-from urllib.request import urlopen
+from urllib.request import urlopen, urljoin
+from re import search
 
 from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from ..settings import USER_AGENT_STRINGS_FILES_DIRS_ROOT_DIR
 
 
 class UserAgentExampleStringsCrawler(ABC):
     host = ''
-    url = ''
+    crawl_url = ''
+    save_dir = ''
+
+    def __init__(self):
+        self.save_dir = os.path.join(
+            USER_AGENT_STRINGS_FILES_DIRS_ROOT_DIR, self.__class__.__name__.replace('Crawler', '').lower()
+        )
+        if not os.path.exists(self.save_dir):
+            os.mkdir(self.save_dir)
 
     @abstractmethod
-    def parse(self, web_resource):
+    def parse(self, beautiful_soup):
         raise NotImplementedError
 
     def crawl(self):
         try:
-            with urlopen(self.url) as web_resource:
+            with urlopen(self.crawl_url) as web_resource:
                 beautiful_soup = BeautifulSoup(web_resource.read(), "html.parser")
         except ValueError as error:
             print(error)
@@ -27,25 +41,38 @@ class UserAgentExampleStringsCrawler(ABC):
 
 class WhatIsMyBrowserCrawler(UserAgentExampleStringsCrawler):
     host = 'https://developers.whatismybrowser.com'
-    url = 'https://developers.whatismybrowser.com/useragents/explore/'
+    crawl_url = 'https://developers.whatismybrowser.com/useragents/explore/'
 
-    # def __init__(self):
-    #     self.url.format('useragents/explore/')
-
-    def parse(self, web_resource):
-        web_resource = web_resource.find('ul', {'id': 'listing-by-field-name'})
-        for li in web_resource.find_all('li'):
-            if li.ul is not None:
-                for lli in li.ul:
-                    a = lli.find('a')
-                    if a != -1:
-                        with urlopen(self.host + a['href']) as header:
-                            with open('res/{}'.format(a.text.lower()), 'w') as out_file:
-                                soup = BeautifulSoup(header.read(), "html.parser")
-                                print()
-                                # for td in soup.find_all('td', {'class': 'useragent'}):
-                                    # print(td)
-                                    # out_file.write(td.a.text.strip(' \t\n\r'))
+    def parse(self, beautiful_soup):
+        for field_name_li in beautiful_soup.find('ul', {'id': 'listing-by-field-name'}).find_all('li'):
+            if field_name_li.ul is not None:
+                for field_values_ul_li in field_name_li.ul:
+                    fld_nm_pg_a = field_values_ul_li.find('a')
+                    if fld_nm_pg_a != -1:
+                        number_of_pages = int(search(r'\d+', str(field_values_ul_li)).group())
+                        print("Number of pages for {} = {}".format(fld_nm_pg_a.text, number_of_pages))
+                        print("Select pages range to download from: ")
+                        try:
+                            start = int(input("start from page: "))
+                            stop = int(input("stop on page: "))
+                            if start <= 0 or stop <= 0 or start > stop:
+                                raise ValueError
+                        except (ValueError, TypeError, KeyboardInterrupt) as error:
+                            if isinstance(error, KeyboardInterrupt):
+                                sys.exit(0)
+                            start = 1
+                            stop = number_of_pages
+                            print(
+                                "Invalid values were provided!",
+                                "Not a correct range of integers!",
+                                "[{}, {}] will be used instead!".format(start, stop)
+                            )
+                        with open(os.path.join(self.save_dir, fld_nm_pg_a.text.lower() + '.txt'), 'w') as out_file:
+                            for page_number in tqdm(range(start, stop + 1), desc="Parsing %s pages" % fld_nm_pg_a.text):
+                                with urlopen(urljoin(self.host, fld_nm_pg_a['href'], str(page_number))) as header:
+                                    soup = BeautifulSoup(header.read(), "html.parser")
+                                    for td in soup.find_all('td', {'class': 'useragent'}):
+                                        out_file.write(td.a.text.strip(' \t\n\r') + '\n')
 
 
 # def fetch_user_agents_lists(url: str):
